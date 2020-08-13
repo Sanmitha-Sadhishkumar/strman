748b45fcd471a95ab2f099f8db919b893ae60fad
Sanmitha-Sadhishkumar <69575577+Sanmitha-Sadhishkumar@users.noreply.github.com>
2020-08-13 20:57:34 +0530

    strmanip.py

--- strmanip/strmanip.py
+++ strmanip/strmanip.py
@@ -1,30 +1,20 @@
 """This module gives access to the functions : 
 	i) word count() - to count the occurrence of each word in the given string
 	ii) addpref() - to add a prefix to each sentence of the given string
-	iii) mirror() - to get the mirror of the given string
-	iv) chrrem() - to remove the mentioned character in the given string
-	v) strcat() - to concatenate two strings
-	vi) swap() - to swap the given strings
-	vii) strrep() - to replace the occurrence of a string in the given string with a new string
-	viii) cwlcount() - to count the occurrence of each character, word and line in the given string
-	ix) slicesub() - gives the step by step slice of the given string
-	x) ismem() - returns whether the second string is found in the first string or not
-	xi) ispalindrome() - returns whether the given string is a palindrome or not
-	xii) vowcons() - returns the number of vowels and consonants in the given string
-	xiii) abece() - returns an abecedarian series
-	xiv) vowrem() - returns a string from the given string with all the vowels removed
-	xv) chrcount() - returns the occurrence of the given character in the given string
-	xvi) pyramid() - returns a pattern of specified condition
+	iii) chrrem() - to remove the mentioned character in the given string
+	iv) strrep() - to replace the occurrence of a string in the given string with a new string
+	v) cwlcount() - to count the occurrence of each character, word and line in the given string
+	vi) slicesub() - gives the step by step slice of the given string
+	vii) ismem() - returns whether the second string is found in the first string or not
+	viii) ispalindrome() - returns whether the given string is a palindrome or not
+	ix) vowcons() - returns the number of vowels and consonants in the given string
+	x) abece() - returns an abecedarian series
+	xi) vowrem() - returns a string from the given string with all the vowels removed
+	xii) chrcount() - returns the occurrence of the given character in the given string
+	xiii) pyramid() - returns a pattern of specified condition
 	xvii) iscapitalised() - used to check whether the given string is in capitalised form or not
-	xviii) istitled() - checks whether the given string is in titled form
-	xix) splitchr() - used to split all the characters in the given string
-	xx) lalpha() - used to get the lowercase English alphabets
-	xxi) ualpha() - used to get the uppercase English alphabets
-	xxii) lalnum() - used to get the lowercase English alphabets with their number (1-26)
-	xxiii) ualnum() - used to get the uppercase English alphabets with their number (1-26)"""
-
-__all__=[wordcount, addpref, mirror, chrrem, strcat, swap, strrep, cwlcount, slicesub, ismem, ispalindrome, vowcons, vowrem, abece, chrcount, iscapitalised, istitled, splitchr, lalpha, ualpha, lalnum, ualnum]
-
+	xiv) istitled() - checks whether the given string is in titled form
+	xv) splitchr() - used to split all the characters in the given string"""
 	
 # word count() - to count the occurrence of each word in the given string
 # parameters - one(a string)
@@ -44,11 +34,6 @@ def addpref(a,b):
         for i in d:
                 e.append(b+i)
         return e
-# mirror() - to get the mirror of the given string
-# input - one(string)
-# output - reverse of the given string
-def mirror(a):
-        return a[::-1]
 
 # chrrem() - to remove the mentioned character in the given string
 # inputs - two (strings - a string and a character to be removed)
@@ -56,18 +41,6 @@ def mirror(a):
 def chrrem(a,b):
         c=a.replace(b,'')
         return c
-
-# strcat() - to concatenate two strings
-# inputs - two strings
-# output - concatenated string formed from the give. strings
-def strcat(a,b):
-        return a+b
-
-# swap() - to swap the given strings
-# inputs - two strings
-# output - swaped strings 
-def swap(a,b):
-        return b,a
 
 # strrep() - to replace the occurrence of a string in the given string with a new string
 # inputs - three strings(a string, string to be replaced, string to be replaced with)
@@ -84,7 +57,6 @@ def strrep(a,b,c):
 # cwlcount() - to count the occurrence of each character, word and line in the given string
 # inputs - a string
 # output - a list containing dictionaries that contain keys as character or word or a line and their occurrence as their values
-
 def cwlcount(a):
         b,c,k=a.split('.'),"",[]
         c=a.replace('.','')
@@ -122,11 +94,11 @@ def ismem(a,b):
 # input - a string
 # output - True / False. True if the given string is a palindrome. False otherwise
 def ispalindrome(a):
-	a = a.lower()
-        if a==a[::-1]:
-                return True
-        else:
-                return False
+    a = a.lower()
+    if a==a[::-1]:
+        return True
+    else:
+        return False
 
 # vowcons() - returns the number of vowels and consonants in the given string
 # input - a string
@@ -221,7 +193,11 @@ def pyramid(a,b,space=False,align='left'):
 # input - a string
 # output - True / False. True if the first character of each line in the given string is in uppercase and rest are in lowercase. False otherwise				
 def iscapitalised(a):
-    if a[0].isupper() and a[1:].islower():
+    b,c=a.split('.'),0
+    for i in b:
+        if i[0].isupper() and i[1:].islower():
+            c+=1
+    if c==len(b):
         return True
     else:
         return False
@@ -255,31 +231,3 @@ def splitchr(a,duplicate=False):
     if duplicate==True:
         e=list(set(d))
         return e
-
-# lalpha() - used to get the lowercase English alphabets
-# input - nil
-# output - a list containing all the lowercase English alphabets as its elements
-def lalpha():
-    a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    return a
-
-# ualpha() - used to get the uppercase English alphabets
-# input - nil
-# output - a list containing all the uppercase English alphabets as its elements
-def ualpha():
-    a=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    return a
-
-# lalnum() - used to get the lowercase English alphabets with their number (1-26)
-# input - nil
-# output - a dictionary containing all the lowercase English alphabets as its keys and their number as their values i.e a - 1, b - 2, etc
-def lalnum():
-    a={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
-    return a
-
-# ualnum() - used to get the uppercase English alphabets with their number (1-26)
-# input - nil
-# output - a dictionary containing all the uppercase English alphabets as its keys and their number as their values i.e A - 1, B - 2, etc
-def ualnum():
-    a={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
-    return a
