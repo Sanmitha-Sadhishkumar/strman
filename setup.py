781a4e048741103f8c692e95631e7a97fa02cfa2
rgn-utya <raagan.uthayaa@protonmail.com>
2020-08-14 20:00:38 +0530

    changed name and made change in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="strmanip", 
+    name="strman", 
     version="0.0.1",
     author="Sanmitha Sadhishkumar",
     author_email="sanmithasadhishkumar@gmail.com",
     description="This module gives additional string functions to make python programming easier than before",
     long_description=long_description,
     long_description_content_type="text/markdown"
-    url="https://github.com/Sanmitha-Sadhishkumar/strmanip/tree/master/strmanip",
+    url="https://github.com/Sanmitha-Sadhishkumar/strman/tree/master/strman",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
--- strman/__init__.py
+++ strman/__init__.py
@@ -1,3 +1,3 @@
-import strmanip
+import strman
 
 #string manipulations.
