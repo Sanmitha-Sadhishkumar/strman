60673651fba6f045babf68598e08d2a029e7aa2a
rgn-utya <68268150+rgn-utya@users.noreply.github.com>
2020-08-14 16:18:16 +0000

    Update setup.py
    changed a small syntax error

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email="sanmithasadhishkumar@gmail.com",
     description="This module gives additional string functions to make python programming easier than before",
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
     url="https://github.com/Sanmitha-Sadhishkumar/strman/tree/master/strman",
     packages=setuptools.find_packages(),
     classifiers=[
