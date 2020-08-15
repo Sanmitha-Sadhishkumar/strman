04a4eb19441019fbe65a7e9d354936c4b265e6ed
Sanmitha-Sadhishkumar <69575577+Sanmitha-Sadhishkumar@users.noreply.github.com>
2020-08-15 17:30:54 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,3 @@
 from strman import *
-wordcount()
-addpref()
-abece()
-chrcount()
-chrrem()
-cwlcount()
-iscapitalised()
-istitled()
-ismem()
-ispalindrome()
-pyramid()
-slicesub()
-splitchr()
-strrep()
-vowcons()
-vowrem()
-wordcount()
-
 
 #string manipulations.
