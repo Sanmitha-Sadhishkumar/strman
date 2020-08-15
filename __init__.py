00885fa9631ef7e7ff664b1b932469d8bbc78477
Sanmitha-Sadhishkumar <69575577+Sanmitha-Sadhishkumar@users.noreply.github.com>
2020-08-15 17:07:36 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from strman import *
+strman.wordcount()
+strman.addpref()
+strman.abece()
+strman.chrcount()
+strman.chrrem()
+strman.cwlcount()
+strman.iscapitalised()
+strman.istitled()
+strman.ismem()
+strman.ispalindrome()
+strman.pyramid()
+strman.slicesub()
+strman.splitchr()
+strman.strrep()
+strman.vowcons()
+strman.vowrem()
+strman.wordcount()
+
+
+#string manipulations.
